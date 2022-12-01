6fed9ac8c5ed8c0a4c3ba4199ab72aea7d13371c
MatheusMartins-1227 <matheus.mrosa@sptech.scholl>
2022-12-01 15:53:38 -0300

    Validando os campos de cadastro e login

--- public/js/cadastro-login.js
+++ public/js/cadastro-login.js
@@ -1,35 +1,65 @@
-const formEntrar = document.querySelector('#entrar')
-const formCadatrar = document.querySelector('#cadastrar')
-const btColor = document.querySelector('.btColor')
+function abaLogar() {
+  entrar.style.left = "25px"
+  cadastrar.style.left = "450px"
+  btColor.style.left = "0px"
+}
 
-document.querySelector('#btLogar')
-  .addEventListener('click', () => {
-    formEntrar.style.left = "25px"
-    formCadatrar.style.left = "450px"
-    btColor.style.left = "0px"
-})
+function abaCadastrar() {
+  entrar.style.left = "-450px"
+  cadastrar.style.left = "25px"
+  btColor.style.left = "110px"
+}
 
-document.querySelector('#btCadastro')
-  .addEventListener('click', () => {
-    formEntrar.style.left = "-450px"
-    formCadatrar.style.left = "25px"
-    btColor.style.left = "110px"
-}) 
-
-
-//  VALIDAÇÃO DE CADASTRO
-
-function validar_cadastro() {
-  var email_cad = input_email_cad.value;
-  var senha_cad = input_senha_cad.value;
-  var conf_senha_cad = input_conf_senha_cad.value;
-
-
-  if(senha_cad != conf_senha_cad || email_cad == ""){
-    alert("ERRO! senhas se diferem")
-  }else {
-    alert("sucesso")
+function invalidEmailChar(email) {
+  const valid_chars = [".","-","_","@","0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var invalid_char = false;
+  for (let index = 0; index < email.length; index++) {
+      const char_email = email[index];
+      if (valid_chars.indexOf(char_email) == -1) {
+          invalid_char = true;
+      }
   }
+  if (invalid_char) {
+      return true;
+  }
+  return false;
+}
 
-  return {entrar}
+// validacao do cadastro
+
+function validarCadastro () {
+  const email = input_email_cad.value.toLowerCase();
+  const senha = input_senha_cad.value;
+  const conf_senha = input_conf_senha_cad.value;
+
+  if (email == "") {
+    alert("Informe seu email");
+    input_email_cad.style.borderColor = "#ff0000";
+  }
+  else if (email.indexOf("@") < 3 || (email.lastIndexOf(".") - email.indexOf("@")) < 4 || (email.length - email.lastIndexOf(".")) < 3 || invalidEmailChar(email)) {
+    alert("Informe um e-mail válido");
+    input_email_cad.style.borderColor = "#ff0000";
+  }
+  else if (senha == "") {
+    alert("informe uma senha")
+  }  
+  else if (senha.length < 5){
+    alert("A senha deve possuir no mínimo 5 caracteres")
+  }
+  else if (senha != conf_senha){
+    alert("senhas se diferem")
+  } 
+}
+
+// validacao do login
+
+function validarLogin() {
+  const email_log = input_email_log.value;
+  const senha_log = input_senha_log.value;
+
+    if (email_log == "" || senha_log == "") {
+      alert("Erro")
+    }else {
+      alert("entrando")
+    }
 }
